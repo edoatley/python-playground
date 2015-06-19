00ef2494fc0fe2de012260a3d35a5c2b4503fad5
edoatley <edoatley@users.noreply.github.com>
2015-06-19 08:00:32 +0100

    Correct spacing and board now printing ok

--- c4.py
+++ c4.py
@@ -1,49 +1,60 @@
-import random
-import copy
-import sys
+from __future__ import print_function
+# import random
+# import copy
+# import sys
 
 BOARDWIDTH = 7
 BOARDHEIGHT = 6
 
-BOARD = [[0 for x in range(BOARDWIDTH)] for y in range(BOARDHEIGHT)] 
+# co-ordinates are always given in the for x, y i.e. across then up
+
+BOARD = [[0 for y in range(BOARDHEIGHT)] for x in range(BOARDWIDTH)] 
 
 def main():
-	print('main')
-	fillATestBoard()
-	#drawBoard()
+    print('main')
+    fillATestBoard()
+    #BOARD[3][2] = 'X'
+    drawBoard()
 
 def fillATestBoard():
-	print('fill')
-	for x in range(BOARDWIDTH + 1):
-		print('x = {0:1d}'.format(x))
-		for y in range(BOARDHEIGHT):	
-			print('y = {0:1d}'.format(y))
-			if ((x + y) % 2) == 1:
-				print("BOARD[{0:1d}][{1:1d}] is X".format(x, y))
-				BOARD[x][y] = 'X'
-			else:
-				print("BOARD[{0:1d}][{1:1d}] is O".format(x, y))
-				BOARD[x][y] = 'O' 
+    print('fill')
+    for x in range(BOARDWIDTH):
+        #print('x = {0:1d}'.format(x))
+        for y in range(BOARDHEIGHT):    
+            #print('y = {0:1d}'.format(y))
+            if ((x + y) % 2) == 1:
+                print("BOARD[{0:1d}][{1:1d}] is X".format(x, y))
+                BOARD[x][y] = 'X'
+            else:
+                print("BOARD[{0:1d}][{1:1d}] is O".format(x, y))
+                BOARD[x][y] = 'O' 
 
-
-def drawBoard():
-    print('draw')
-    for x in range(1, BOARDWIDTH + 1):
-        print(' %s  ' % x)
-    print()
-
+def drawStructuralRow():
     print('+---+' + ('---+' * (BOARDWIDTH - 1)))
 
+def drawRow(rowNum):
+    #print('drawRow(' + str(rowNum) + ')')
+    for x in range(BOARDWIDTH):
+        print('| {0} '.format(BOARD[x][rowNum]), end='')
+    print('|')
+
+def drawBoard():
+    print('drawBoard')
+    
+    for x in range(BOARDWIDTH):
+        print('  {0} '.format(x + 1), end='')
+    print('')
+    drawStructuralRow()
+
     for y in range(BOARDHEIGHT):
-        print('|   |' + ('   |' * (BOARDWIDTH - 1)))
+        #print('|   |' + ('   |' * (BOARDWIDTH - 1)))
 
-        print('|')
-        for x in range(BOARDWIDTH):
-            print(' %s |' % BOARD[x][y])
-        print()
+        drawRow(y)    
+        #print()
 
-        print('|   |' + ('   |' * (BOARDWIDTH - 1)))
+        #print('|   |' + ('   |' * (BOARDWIDTH - 1)))
+        drawStructuralRow()
 
-        print('+---+' + ('---+' * (BOARDWIDTH - 1)))
+        
 
 main()
